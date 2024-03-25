ad9fa7284a739c071fac0ec0bebc69fbe5a43206
Akash Kola <akashkola321@gmail.com>
2024-03-25 16:22:23 +0530

    added global error handling
    added buber dinner problem details factory to add extensions to the problem json

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,7 @@
+using BuberDinner.Api.Common.Errors;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -9,10 +11,13 @@ var builder = WebApplication.CreateBuilder(args);
 
     // builder.Services.AddInfrastructure();
     builder.Services.AddControllers();
+
+    builder.Services.AddSingleton<ProblemDetailsFactory, BuberDinnerProblemDetailsFactory>();
 }
 
 var app = builder.Build();
 {
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
